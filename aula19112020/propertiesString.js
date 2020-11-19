5e68accf207ba8474b2a12f60db80196205f9f6f
Sinioh <matheus.23d@hotmail.com>
2020-11-19 16:50:09 -0300

    Terceiro Commit Aula 19112020

---
+++ aula19112020/propertiesString.js
@@ -0,0 +1,11 @@
+// Propriedades e Métodos da String
+
+// Length
+
+console.log('Isso é uma string'.length);
+
+console.log("Texto Maiúsculo para MinúSCUlo".toLowerCase());
+
+console.log("Texto Maiúsculo para MinúSCUlo".toUpperCase());
+
+console.log("         Preparando texto planilha    ".trim())
