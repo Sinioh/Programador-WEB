ad42f38efd6a69f93944761647d5a23859a89153
Sinioh <matheus.23d@hotmail.com>
2020-11-19 16:28:36 -0300

    Segundo Commit aula 19112020

--- aula19112020/typeString.js
+++ aula19112020/typeString.js
@@ -1,1 +1,33 @@
-// Strings em JavaScript
+// Tipos de dados utilizados pelo JS
+
+// Strings em JavaScript 
+
+console.log('Isso é uma string');
+console.log("Isso é outra string");
+console.log(`Isso é mais uma string`);
+
+console.log("It's six o'clock");
+
+console.log(`Observe nessa "String"`);
+console.log('Observe nessa outra "String"');
+
+// Como imprimir caracteres especiais
+/*
+'
+"
+\
+quebra de linha
+retorno de carro 
+tabulação vertical
+tabulação
+backspace
+*/
+
+console.log("\'")
+console.log("\"")
+console.log("\\")
+console.log("Como imprimir \ncaracteres especiais") // \
+console.log("Como imprimir \rcaracteres especiais") // \r
+console.log("Como imprimir \vcaracteres especiais") // \v
+console.log("Como imprimir \tcaracteres especiais") // \t
+console.log("Como imprimir \bcaracteres especiais") // \b
