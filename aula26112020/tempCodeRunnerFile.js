db50352fd227a2e2d76c993c5f331623dfeef3e5
Sinioh <matheus.23d@hotmail.com>
2020-11-27 15:23:47 -0300

    commit aula27112020

--- aula26112020/tempCodeRunnerFile.js
+++ aula26112020/tempCodeRunnerFile.js
@@ -1,6 +1,4 @@
-let x = 10, y = '5';
-if (x == 10 || y === 5) {
-    console.log('Teste lógico verdadeiro');
-} else {
-    console.log('Teste lógico falso');
-}
+
+console.log(20 === '20');
+
+console.log(10 !== '10');
---
+++ aula27112020/exemplosHTML/js/script.js
@@ -0,0 +1,7 @@
+// Meu Código
+let numA = 15;
+let numB = 8;
+let resultado;
+resultado = numA + numB;
+console.log('Resultado:', resultado);
+document.write('<h2> Resultado: ' + resultado + "</h2>");    
