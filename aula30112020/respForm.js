ad16e13985e5ed591cd105b38fdd44889cb61933
Sinioh <matheus.23d@hotmail.com>
2020-11-30 16:51:48 -0300

    commit aula 30112020

---
+++ aula30112020/respForm.js
@@ -0,0 +1,5 @@
+let x = 6, y = 5, z = x + y; document.write(z + "<br>"); // 11
+z = x - y; document.write(z + "<br>"); // 1
+z = x * y; document.write(z + "<br>"); // 30
+z = x / y; document.write(z + "<br>"); // 1.2
+let peso = 65, altura = 1.70 , IMC = peso / (altura * altura); document.write(IMC); // 22.49134948096886
---
+++ aula30112020/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+o); console.log(IMC)
